1566f71871168d33d3eb1a956cf6abb796093da8
Mateus <mateusdesouzaroseno@gmail.com>
2025-06-06 14:17:02 -0300

    Create Client.py

---
+++ python/Client_serv_multi/Client.py
@@ -0,0 +1,48 @@
+import socket
+import threading
+
+def receber_mensagens(socket_client):
+    while True:
+        try:
+            mensagem = socket_client.recv(1024).decode('utf-8')
+            print("\n" + mensagem +"\ndigite mensagem", end="")
+        except:
+            print("\nconexão com o servidor perdida")
+            break
+
+def iniciar_cliente():
+    host = "127.0.0.1"
+    porta = 12349
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host,porta))
+
+        print(s.recv(1024).decode('utf-8'), end="")
+        usuario = input("a ")
+        s.sendall(usuario.encode('utf-8'))
+
+        print(s.recv(1024).decode('utf-8'))
+        senha = input("b ")
+        s.sendall(senha.encode('utf-8'))
+
+        resposta = s.recv(1024).decode('utf-8')
+        if resposta != "AUTENTICACAO OK":
+            print("falha na autenticação")
+            return
+        
+        print("autenticaçao com sucesso! comandos disponiveis:")
+        print("HORA - solicitar a hora atual")
+        print("DATA - solicitar data atual")
+        print("@usuario mensagem - mensagem privada")
+
+        thread_receber = threading.Thread(target=receber_mensagens, args=(s,))
+        thread_receber.start()
+
+        while True:
+            mensagem = input("msg; ")
+            if mensagem.lower() == "sair":
+                break
+            s.sendall(mensagem.encode('utf-8'))
+            
+if __name__ == "__main__":
+    iniciar_cliente()
