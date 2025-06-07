678f8944201063ac674069dcd0dde8f3d7d8f793
Mateus <mateusdesouzaroseno@gmail.com>
2025-06-07 13:43:48 -0300

    Atualizar o Serve.py

--- python/Client_serv_multi/Serve.py
+++ python/Client_serv_multi/Serve.py
@@ -2,31 +2,26 @@ import socket
 import threading
 import time
 from datetime import datetime
-
 HOST='123.0.0.1'
 PORTA= 12349
 USUARIOS={"aluno": "ifpb1234","admin": "admin123"}
 LOGS_FILE="serve_logs.txt"
 clientes_conectados =[]
-
 def registrar_log(mensagem):
     with open(LOGS_FILE,"a") as log_file:
         log_file.write(f"[{datetime.now()}]{mensagem}\n")
-
 def broadcast (mensagem, origem=None):
     for cliente in clientes_conectados:
         if cliente != origem:
             try:
-cliente.sendall(mensagem.encode('utf-8'))
-            except:
-clientes_conectados.remove(clientes_conectados)
-        
+                cliente.sendall(mensagem.encode('utf-8'))
+            except:        
+                clientes_conectados.remove(clientes_conectados)        
 def handle_cliente(conn, addr):
     try:
         conn.sendall("digite usuario: ". encode('utf-8'))        
         usuario= conn.recv(1234).decode('utf-8').strip()
         conn.sendall("Digite senha:".encode('utf_8'))
-
         senha= conn.recv(1034).decode('utf-8').strip
         if USUARIOS.get(usuario) != senha:
             conn.sendall("AUTENTICACAO FALHOU".encode('utf-8'))
