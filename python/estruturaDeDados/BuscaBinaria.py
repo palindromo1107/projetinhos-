385c1503405dc8efa37f349722a9bd6a2581b4b4
Mateus <mateusdesouzaroseno@gmail.com>
2025-08-30 12:16:06 -0300

    Atualizar o BuscaBinaria.py

--- python/estruturaDeDados/BuscaBinaria.py
+++ python/estruturaDeDados/BuscaBinaria.py
@@ -2,52 +2,67 @@
 import time
 
 #lista
-l = [1,2,3,4,5,6,7,8,11]
+l = [1,2,3,4,5,6,7,8,9]
 
-def busca(lista):
+def busca(lista, alvo):
     
-    #item inicio da lista
-    inicio = lista [0]
+    #definição inicio da lista
+    inicio = 0
     
-    #item final da lista
-    final = lista [ len(lista) - 1 ]
+    #definição final da lista
+    final = len(lista) - 1
     
-    #item meio da lista
-    meio = len(lista) // 2
-    
-    #auxiliar
-    aux = lista[meio]
-    
-    cond = True
-    
-    #definir o item
-    num = int(input())
-    
-    #verificar se está na lista
-    if num not in lista:
-        print("numero fora da lista")
+    while inicio < final:
         
-        #buscar o item
-    else:
-        while cond:
+        #definição meio da lista
+        meio = (inicio + final) // 2
+        
+#_______________________________________#
+    #extra
+        time.sleep(1.5)
+        
+        lis = []
+        lis.append(lista[inicio : final])
+        
+        print()
+        print(meio)
+        print()
+        print(lis)
+        print()
+        #esta parte é meio confusa mas funciona
+        #nao entendo o porque
+#_______________________________________#
+        
+        #verificar se o item está no meio
+        if alvo != lista [ meio ]:
             
-            #verificar se encontrou
-            if num == aux:
-                cond = False
-                print(aux)
+            #verificar se está a esquerda
+            if alvo < lista[meio]:
                 
-                #caso seja menor que o auxiliar
-            elif num <= aux:
-                meio -= 1
-                aux = lista [meio]
+                #o fim da lista se move para a esquerda e fica após o meio
+                final = meio - 1
                 
-                #caso seja maior que o auxiliar
+            #se nao estiver está a direita
             else:
-                meio += 1
-                aux = lista [meio]
                 
-                #rota percorrida pelo auxiliar parra encontrar o item
-            time.sleep(1)
-            print(aux)
+                #o inicio se move para a direita e fica após o meio
+               inicio = meio + 1
+               
+        #quando o item estiver no meio
+        else:
+            
+            #retorna o valor do item
+            return meio
+            
+    #retorna o index do item
+    return -1
     
-busca(l)
+    #alvo
+target = 8
+
+#executar função e atribuir a variável a
+a = busca(l, target)
+
+print()
+print(f"item {target} encontrado no index {a}")
+print()
